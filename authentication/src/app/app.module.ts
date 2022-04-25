589c663ec2e6c81c368b74b1d2be0546025d8d24
bharath857 <bharath.reddy.857@gmail.com>
2022-04-25 22:37:10 +0530

    separated auth module

--- authentication/src/app/app.module.ts
+++ authentication/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './authenticate/login/login.component';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { FirstPageSecoundChildComponent } from './first-page/first-page-secound-child/first-page-secound-child.component';
 import { FirstPageFirstChildComponent } from './first-page/first-page-first-child/first-page-first-child.component';
@@ -13,11 +12,11 @@ import { SecoundPageComponent } from './secound-page/secound-page.component';
 import { SecoundPageFirstChildComponent } from './secound-page/secound-page-first-child/secound-page-first-child.component';
 import { SecoundPageSecoundChildComponent } from './secound-page/secound-page-secound-child/secound-page-secound-child.component';
 import { AuthInterceptor } from './authenticate/interceptor/auth.interceptor';
+import { AuthModule } from './authenticate/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     FirstPageComponent,
     FirstPageSecoundChildComponent,
     FirstPageFirstChildComponent,
@@ -30,7 +29,8 @@ import { AuthInterceptor } from './authenticate/interceptor/auth.interceptor';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [ 
     {
